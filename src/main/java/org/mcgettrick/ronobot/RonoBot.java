fad1b7d5685f7b3ed31d714ddc64f258e03cf797
Nathan Stien <npstien@ilstu.edu>
2012-11-08 17:08:59 -0600

    use a prefix on the command

--- src/main/java/org/mcgettrick/ronobot/RonoBot.java
+++ src/main/java/org/mcgettrick/ronobot/RonoBot.java
@@ -10,7 +10,7 @@ public class RonoBot extends PircBot {
 
     public void onMessage(String channel, String sender,
                         String login, String hostname, String message) {
-        if (message.equalsIgnoreCase("time")) {
+        if (message.equalsIgnoreCase("!time")) {
             String time = new java.util.Date().toString();
             sendMessage(channel, sender + ": The time is now " + time);
         }
