20d8f7b58e6eb16259e218a57b4a8c4a3c54645b
Nathan Stien <npstien@ilstu.edu>
2012-11-08 15:57:46 -0600

    making a starter project for ron's bot

---
+++ src/main/java/org/mcgettrick/ronobot/RonoBotMain.java
@@ -0,0 +1,19 @@
+import org.jibble.pircbot.*;
+
+public class RonoBotMain {
+    
+    public static void main(String[] args) throws Exception {
+        
+        // Now start our bot up.
+        RonoBot bot = new RonoBot();
+        
+        // Enable debugging output.
+        bot.setVerbose(true);
+        
+        // Connect to the IRC server.
+        bot.connect("irc.freenode.net");
+
+        // Join the #pircbot channel.
+        bot.joinChannel("#silly-bot-test");
+    }
+}
